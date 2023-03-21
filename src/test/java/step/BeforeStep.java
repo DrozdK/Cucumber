ef5c69762ac3e9e1d50932edef4b6c16d2c937a9
Karalina199 <carolina.chernenok@gmail.com>
2023-03-21 23:19:17 +0300

    add constants

--- src/test/java/step/BeforeStep.java
+++ src/test/java/step/BeforeStep.java
@@ -6,12 +6,14 @@ import com.codeborne.selenide.Selenide;
 import io.cucumber.java.en.Given;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import static constants.Constants.TimeoutVariable.IMPLICIT_WAIT;
+
 public class BeforeStep {
 
     @Given("Открываем сайт {string}")
     public void openWebSite(String url) {
         Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
-        Configuration.timeout = 60000;
+        Configuration.timeout = IMPLICIT_WAIT;
         Selenide.open(url);
     }
 }
