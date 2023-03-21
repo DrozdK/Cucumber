b034e00c721253c55da479aa729e5336995c5ac0
Karalina199 <carolina.chernenok@gmail.com>
2023-03-21 23:11:14 +0300

    refactor hw7

---
+++ src/test/java/RunTests.java
@@ -0,0 +1,16 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/scenarios",
+        glue = "step",
+        snippets = CAMELCASE,
+        plugin = { "pretty", "html:target/Homework7.html" }
+)
+
+public class RunTests {
+}
